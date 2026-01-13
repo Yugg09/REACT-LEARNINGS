c5e83c70cfc4af140077c2b0ec017493a98f0fce
Yug <yugchoudhary947@gmail.com>
2026-01-13 15:31:59 +0530

    test push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+
+
 export const App = () => {
-  return <h1>hello,yug</h1>; // html like code in js file is jsx
+  return ( 
+   <div>
+   <h2>age : queen of tears</h2> // html like code in js file is jsx
+  <h3>Rating:8.2</h3>
+  <p>best movie i watched</p>
+
+</div>
+  )
 };
  
-<h1>hello,yug</h1>;
+{/* <h1>hello,yug</h1>;
 React.createElement("h1",null,"hello,yug")
-console.log(React.createElement("h1",null,"hello,yug"));
+console.log(React.createElement("h1",null,"hello,yug")); */}
+
+// every jsx must have one parent element, if try to return multiple elements return throw
+
+// every jsx nees to be closed
 
