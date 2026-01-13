257d8a51732fa19a064994f250d23f03e45e454d
Yug <yugchoudhary947@gmail.com>
2026-01-13 15:28:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return <h1>hello,yug</h1>; // html like code in js file is jsx
+};
+ 
+<h1>hello,yug</h1>;
+React.createElement("h1",null,"hello,yug")
+console.log(React.createElement("h1",null,"hello,yug"));
+
